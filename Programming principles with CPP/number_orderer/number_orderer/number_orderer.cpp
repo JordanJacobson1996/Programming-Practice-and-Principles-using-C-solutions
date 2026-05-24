0609bccebc18829a5de1bdcf7a29a2d9cc8cd32f
Jordan Jacobson <jordandurjacobson@gmail.com>
2026-05-25 07:27:31 +1000

    Finished program to sort three integers entered by user from smallest to largest

--- Programming principles with CPP/number_orderer/number_orderer/number_orderer.cpp
+++ Programming principles with CPP/number_orderer/number_orderer/number_orderer.cpp
@@ -8,6 +8,7 @@ int main()
     int integer_one;
     int integer_two;
     int integer_three;
+    int temp_int = 0;
 
     std::cout << "Please enter three integers: \n";
     std::cout << "Integer one: ";
@@ -16,4 +17,24 @@ int main()
     std::cin >> integer_two;
     std::cout << "Integer three: ";
     std::cin >> integer_three;
+
+    while (integer_one > integer_two || integer_two > integer_three)
+    {
+        if (integer_one > integer_two)
+        {
+            temp_int = integer_one;
+            integer_one = integer_two;
+            integer_two = temp_int;
+        }
+        else if (integer_two > integer_three)
+        {
+            temp_int = integer_two;
+            integer_two = integer_three;
+            integer_three = temp_int;
+        }
+    }
+
+    std::cout << integer_one << " ";
+    std::cout << integer_two<<" ";
+    std::cout << integer_three;
 }
