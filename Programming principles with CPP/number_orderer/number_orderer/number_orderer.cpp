ffd46c57b3ea993e2257136491b8dd666910bf44
Jordan Jacobson <jordandurjacobson@gmail.com>
2026-05-25 06:50:13 +1000

    Created number sorting program from exerciuse six, chapter 2.

---
+++ Programming principles with CPP/number_orderer/number_orderer/number_orderer.cpp
@@ -0,0 +1,19 @@
+// number_orderer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int integer_one;
+    int integer_two;
+    int integer_three;
+
+    std::cout << "Please enter three integers: \n";
+    std::cout << "Integer one: ";
+    std::cin >> integer_one;
+    std::cout << "Integer two: ";
+    std::cin >> integer_two;
+    std::cout << "Integer three: ";
+    std::cin >> integer_three;
+}
